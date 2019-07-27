54516859e7631aa3ad010580e171d321b4b9e162
Michelle <mia_araujo92@hotmail.com>
2019-07-27 09:41:06 -0300

    configuração dos sons e layout

--- app/src/main/java/com/example/aprendaingles/fragments/BichosFragment.java
+++ app/src/main/java/com/example/aprendaingles/fragments/BichosFragment.java
@@ -1,109 +1,90 @@
 package com.example.aprendaingles.fragments;
 
-import android.content.Context;
-import android.net.Uri;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import com.example.aprendaingles.R;
+import com.example.aprendaingles.activity.MainActivity;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link BichosFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link BichosFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class BichosFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class BichosFragment extends Fragment implements View.OnClickListener {
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private ImageButton ImageButtonDog, ImageButtonCat, ImageButtonLion, ImageButtonMonkey,
+            ImageButtonOvelha, ImageButtonCow;
 
-    private OnFragmentInteractionListener mListener;
+    private MediaPlayer mediaPlayer;
 
     public BichosFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BichosFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static BichosFragment newInstance(String param1, String param2) {
-        BichosFragment fragment = new BichosFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_bichos, container, false);
-    }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+        View view = inflater.inflate(R.layout.fragment_bichos, container, false);
+
+        ImageButtonDog = view.findViewById(R.id.imageButtonDog);
+        ImageButtonCat = view.findViewById(R.id.imageButtonCat);
+        ImageButtonLion = view.findViewById(R.id.imageButtonLion);
+        ImageButtonMonkey = view.findViewById(R.id.imageButtonMonkey);
+        ImageButtonOvelha = view.findViewById(R.id.imageButtonOvelha);
+        ImageButtonCow = view.findViewById(R.id.imageButtonCow);
+
+        ImageButtonDog.setOnClickListener(this);
+        ImageButtonCat.setOnClickListener(this);
+        ImageButtonLion.setOnClickListener(this);
+        ImageButtonMonkey.setOnClickListener(this);
+        ImageButtonOvelha.setOnClickListener(this);
+        ImageButtonCow.setOnClickListener(this);
+
+        return view;
     }
 
     @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.imageButtonDog:
+                mediaPlayer = MediaPlayer.create(getActivity(),R.raw.dog);
+                tocarSom();
+                 break;
+            case R.id.imageButtonCat:
+                mediaPlayer = MediaPlayer.create(getActivity(),R.raw.cat);
+                tocarSom();
+                break;
+            case R.id.imageButtonLion:
+                mediaPlayer = MediaPlayer.create(getActivity(),R.raw.lion);
+                tocarSom();
+                break;
+            case R.id.imageButtonMonkey:
+                mediaPlayer = MediaPlayer.create(getActivity(),R.raw.monkey);
+                tocarSom();
+                break;
+            case R.id.imageButtonOvelha:
+                mediaPlayer = MediaPlayer.create(getActivity(),R.raw.sheep);
+                tocarSom();
+                break;
+            case R.id.imageButtonCow:
+                mediaPlayer = MediaPlayer.create(getActivity(),R.raw.cow);
+                tocarSom();
+                break;
+
         }
     }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
+    public void tocarSom(){
+        if(mediaPlayer != null){
+            mediaPlayer.start();
+            mediaPlayer.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+                @Override
+                public void onCompletion(MediaPlayer mp) {
+                    mp.release();
+                }
+            });
+        }
     }
 }
